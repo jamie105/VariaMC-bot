493843bd4b7befc5b932dfeae53d06a2fbf4714f
jamie105 <jamie.morgan105@gmail.com>
2022-06-06 15:21:34 +0200

    fck antiviruses

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.once('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("INITIAL DEVELOPMENT", {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/variamc"
+    });
+})
+
+client.login('OTgzMjYwNTMwMDExNjkzMDU2.GpqLue.Q4vTBEfQgwbf5u4ssqgLjpANXZuaZd3ql8GWio')

0448ee311efd5e4b917198d75360b01daafbb6a8
jamie105 <43914329+jamie105@users.noreply.github.com>
2022-06-06 15:48:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.once('ready', async () => {
 })
 
 client.on('message', async message => {
-    if(message.startsWith('<@983260530011693056> ') && !message.author.bot){
+    if(message.content.startsWith('<@983260530011693056> ') && !message.author.bot){
         let json = await fetch("https://api.affiliateplus.xyz/api/chatbot?message="+message.content.replace('<@983260530011693056> ','')+"&name=Vamix&user=" + message.author.id + "variamcbot")
         let chat = await json.json()
         let messagemain = chat.message.replaceAll('@everyone','#everyone').replaceAll('@here','#here')

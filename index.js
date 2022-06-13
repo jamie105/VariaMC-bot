d82581a77c937b2e5b396acc1a2e4fe4273cd5f3
jamie105 <43914329+jamie105@users.noreply.github.com>
2022-06-13 10:02:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const Discord = require('discord.js');
 const fetch = require("node-fetch")
 const client = new Discord.Client();
+const cooldowns = new Discord.Collection();
+
+//Setup Collections For Organisation Files
+client.commands = new Discord.Collection();
+client.constants = new Discord.Collection();
+
+
+//Find Organisation Files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+const loggingFiles = fs.readdirSync('./logging').filter(file => file.endsWith('.js'));
+for (const file of loggingFiles) {
+    const logging = require(`./logging/${file}`);
+    client.logging.set(logging.name, logging);
+}
 
 client.once('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -16,227 +34,227 @@ client.on('message', async message => {
 })
 
 client.on('channelCreate', async channel => {
-    console.log(channel)
+    client.logging.get('channelCreate').execute(client, channel)
 })
 
 
 client.on('channelDelete', async channel => {
-    console.log(channel)
+    client.logging.get('channelDelete').execute(client, channel)
 })
 
 
 client.on('channelUpdate', async channel => {
-    console.log(channel)
+    client.logging.get('channelUpdate').execute(client, channel)
 })
 
 
 client.on('channelPinsUpdate', async channel => {
-    console.log(channel)
+    client.logging.get('channelPinsUpdate').execute(client, channel)
 })
 
 
 client.on('emojiCreate', async emoji => {
-    console.log(emoji)
+    client.logging.get('emojiCreate').execute(client, emoji)
 })
 
 
 client.on('emojiDelete', async emoji => {
-    console.log(emoji)
+    client.logging.get('emojiDelete').execute(client, emoji)
 })
 
 
 client.on('emojiUpdate', async emoji => {
-    console.log(emoji)
+    client.logging.get('emojiUpdate').execute(client, emoji)
 })
 
 
 client.on('guildBanAdd', async ban => {
-    console.log(ban)
+    client.logging.get('guildBanAdd').execute(client, ban)
 })
 
 
 client.on('guildBanRemove', async ban => {
-    console.log(ban)
+    client.logging.get('guildBanRemove').execute(client, ban)
 })
 
 
 client.on('guildIntegrationsUpdate', async guild => {
-    console.log(guild)
+    client.logging.get('guildIntegrationsUpdate').execute(client, guild)
 })
 
 
 client.on('guildMemberAdd', async member => {
-    console.log(member)
+    client.logging.get('guildMemberAdd').execute(client, member)
 })
 
 
 client.on('guildMemberRemove', async member => {
-    console.log(member)
+    client.logging.get('guildMemberRemove').execute(client, member)
 })
 
 
 client.on('guildMemberUpdate', async member => {
-    console.log(member)
+    client.logging.get('guildMemberUpdate').execute(client, member)
 })
 
 
 client.on('guildUpdate', async guild => {
-    console.log(guild)
+    client.logging.get('guildUpdate').execute(client, guild)
 })
 
 
 client.on('inviteCreate', async invite => {
-    console.log(invite)
+    client.logging.get('inviteCreate').execute(client, invite)
 })
 
 
 client.on('inviteDelete', async invite => {
-    console.log(invite)
+    client.logging.get('inviteDelete').execute(client, invite)
 })
 
 
 client.on('messageCreate', async message => {
-    console.log(message)
+    client.logging.get('messageCreate').execute(client, message)
 })
 
 
 client.on('messageDelete', async message => {
-    console.log(message)
+    client.logging.get('messageDelete').execute(client, message)
 })
 
 
 client.on('messageDeleteBulk', async message => {
-    console.log(message)
+    client.logging.get('messageDeleteBulk').execute(client, message)
 })
 
 
 client.on('messageReactionRemoveAll', async reaction => {
-    console.log(reaction)
+    client.logging.get('messageReactionRemoveAll').execute(client, reaction)
 })
 
 
 client.on('messageReactionRemoveEmoji', async reaction => {
-    console.log(reaction)
+    client.logging.get('messageReactionRemoveEmoji').execute(client, reaction)
 })
 
 
 client.on('messageReactionAdd', async reaction => {
-    console.log(reaction)
+    client.logging.get('messageReactionAdd').execute(client, reaction)
 })
 
 
 client.on('messageReactionRemove', async reaction => {
-    console.log(reaction)
+    client.logging.get('messageReactionRemove').execute(client, reaction)
 })
 
 
 client.on('messageUpdate', async message => {
-    console.log(message)
+    client.logging.get('messageUpdate').execute(client, message)
 })
 
 
 client.on('presenceUpdate', async presence => {
-    console.log(presence)
+    client.logging.get('presenceUpdate').execute(client, presence)
 })
 
 
 client.on('rateLimit', async rate => {
-    console.log(rate)
+    client.logging.get('rateLimit').execute(client, rate)
 })
 
 
 client.on('roleCreate', async role => {
-    console.log(role)
+    client.logging.get('roleCreate').execute(client, role)
 })
 
 
 client.on('roleDelete', async role => {
-    console.log(role)
+    client.logging.get('roleDelete').execute(client, role)
 })
 
 
 client.on('roleUpdate', async role => {
-    console.log(role)
+    client.logging.get('roleUpdate').execute(client, role)
 })
 
 
 client.on('threadCreate', async thread => {
-    console.log(thread)
+    client.logging.get('threadCreate').execute(client, thread)
 })
 
 
 client.on('threadDelete', async thread => {
-    console.log(thread)
+    client.logging.get('threadDelete').execute(client, thread)
 })
 
 
 client.on('threadListSync', async thread => {
-    console.log(thread)
+    client.logging.get('threadListSync').execute(client, thread)
 })
 
 
 client.on('threadMemberUpdate', async thread => {
-    console.log(thread)
+    client.logging.get('threadMemberUpdate').execute(client, thread)
 })
 
 
 client.on('threadMembersUpdate', async thread => {
-    console.log(thread)
+    client.logging.get('threadMembersUpdate').execute(client, thread)
 })
 
 
 client.on('threadUpdate', async thread => {
-    console.log(thread)
+    client.logging.get('threadUpdate').execute(client, thread)
 })
 
 
 client.on('typingStart', async typing => {
-    console.log(typing)
+    client.logging.get('typingStart').execute(client, typing)
 })
 
 
 client.on('userUpdate', async user => {
-    console.log(user)
+    client.logging.get('userUpdate').execute(client, user)
 })
 
 
 client.on('voiceStateUpdate', async voice => {
-    console.log(voice)
+    client.logging.get('voiceStateUpdate').execute(client, voice)
 })
 
 
 client.on('webhookUpdate', async webhook => {
-    console.log(webhook)
+    client.logging.get('webhookUpdate').execute(client, webhook)
 })
 
 
 client.on('stageInstanceCreate', async stage => {
-    console.log(stage)
+    client.logging.get('stageInstanceCreate').execute(client, stage)
 })
 
 
 client.on('stageInstanceUpdate', async stage => {
-    console.log(stage)
+    client.logging.get('stageInstanceUpdate').execute(client, stage)
 })
 
 
 client.on('stageInstanceDelete', async stage => {
-    console.log(stage)
+    client.logging.get('stageInstanceDelete').execute(client, stage)
 })
 
 
 client.on('stickerCreate', async sticker => {
-    console.log(sticker)
+    client.logging.get('stickerCreate').execute(client, sticker)
 })
 
 
 client.on('stickerDelete', async sticker => {
-    console.log(sticker)
+    client.logging.get('stickerDelete').execute(client, sticker)
 })
 
 
 client.on('stickerUpdate', async sticker => {
-    console.log(sticker)
+    client.logging.get('stickerUpdate').execute(client, sticker)
 })
 
 

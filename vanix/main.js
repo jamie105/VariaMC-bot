32f25d16e5b118774f0c26cea3339ca6007f0165
jamie105 <jamie.morgan105@gmail.com>
2022-07-13 23:07:05 +0200

    push

--- vanix/main.js
+++ vanix/main.js
@@ -7,8 +7,8 @@ module.exports = {
     name: "main",
     description: "the core for vanix",
     async execute(client, message) {
-		if(message.content.startsWith("//")){return}
-		var modifiedmessage = message.content
+		if(!message.content.startsWith("//")){return}
+		var modifiedmessage = message.content.replaceAll("//","")
 		//replace items in message
 		for(const x in chatcheck.replace){
 			if(modifiedmessage.includes(x)){
@@ -28,7 +28,7 @@ module.exports = {
 			}
 		}
 		
-        var json = await fetch("https://api.affiliateplus.xyz/api/chatbot?message="+modifiedmessage+"&name=Vanix&user=siryakstudioschatbot"+message.author.id+"&botmaster=<@462554041440796674>&master=<@462554041440796674>")
+        var json = await fetch("https://api.affiliateplus.xyz/api/chatbot?message="+modifiedmessage+"&name=Vanix&user=siryakstudioschatbot001"+message.author.id+"&botmaster=<@462554041440796674>&master=<@462554041440796674>")
 		var chat = await json.json()
 		message.reply(chat.message)
     }

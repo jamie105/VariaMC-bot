7bf84e4779e18eaadbf551b871f369ef42ebeb9b
jamie105 <jamie.morgan105@gmail.com>
2022-07-13 21:38:23 +0200

    directory updates

--- index.js
+++ index.js
@@ -1,39 +1,140 @@
+//General
 const Discord = require('discord.js');
 const fetch = require("node-fetch")
 const client = new Discord.Client();
 const cooldowns = new Discord.Collection();
 const fs = require('fs');
+const stopPhishing = require('stop-discord-phishing')
+
+//config files
+client.assets = require('./config/assets.json')
+
+//SpecificVariables
+const prefix = client.assets.prefix
 
 //Setup Collections For Organisation Files
+client.constants = new Discord.Collection();
 client.commands = new Discord.Collection();
+client.database = new Discord.Collection();
 client.logging = new Discord.Collection();
-
+client.vanix = new Discord.Collection();
 
 //Find Organisation Files
+const constantFiles = fs.readdirSync('./constants').filter(file => file.endsWith('.js'));
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const databasesFiles = fs.readdirSync('./database').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const loggingsFiles = fs.readdirSync('./logging').filter(file => file.endsWith('.js'));
+const vanixsFiles = fs.readdirSync('./vanix').filter(file => file.endsWith('.js'));
+console.log(vanixsFiles)
+
+for (const file of constantFiles) {
+    const constant = require(`./constants/${file}`);
+    client.constants.set(constant.name, constant);
+}
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
-const loggingFiles = fs.readdirSync('./logging').filter(file => file.endsWith('.js'));
-for (const file of loggingFiles) {
+for (const file of databasesFiles) {
+    const databases = require(`./database/${file}`);
+    client.database.set(databases.name, databases);
+}
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    client.events.set(event.name, event);
+}
+for (const file of loggingsFiles) {
     const loggings = require(`./logging/${file}`);
     client.logging.set(loggings.name, loggings);
 }
+for (const file of vanixsFiles) {
+    const vanixs = require(`./vanix/${file}`);
+    client.vanix.set(vanixs.name, vanixs);
+    console.log(vanixs.name)
+}
+
 
 client.once('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("INITIAL DEVELOPMENT", {
+    /**setInterval(function(){
+	for(let x of client.constants){
+		console.log(x)
+	}
+	},5000);**/
+    client.user.setActivity("variamc.net", {
         type: "STREAMING",
         url: "https://www.twitch.tv/variamc"
     });
 })
 
+console.log(client.vanix.get("main"))
+
 client.on('message', async message => {
     if(message.author.bot){return}
-    
+    if(message.channel.id===client.assets.channels.specific.vanix){
+		return client.vanix.get("main").execute(client, message)
+	}
+    if(!message.content.startsWith(prefix)){return}
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) {} else {
+        if (!cooldowns.has(command.name)) {
+            cooldowns.set(command.name, new Discord.Collection());
+        }
+        const now = Date.now();
+        const timestamps = cooldowns.get(command.name);
+        const cooldownAmount = (command.cooldown || 3) * 1000;
+        if (timestamps.has(message.author.id)) {
+            const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+            if (now < expirationTime) {
+                const timeLeft = (expirationTime - now) / 1000;
+                message.channel.bulkDelete(1)
+                    .then(async () => {
+                        var noargs = {
+                            'title': '__**Command Cooldown**__',
+                            'description': 'Incorrect Command Usage',
+                            'color': 16711712,
+                            'footer': {
+                                'icon_url': 'https://cdn.discordapp.com/avatars/759867790416019497/c440976d3f1a5da5c62aaa3151aeb1e7.webp?size=32',
+                                'text': 'GalacticLands - //help'
+                            },
+                            'author': {
+                                'name': 'GalacticLands',
+                                'icon_url': 'https://cdn.discordapp.com/avatars/759867790416019497/c440976d3f1a5da5c62aaa3151aeb1e7.webp?size=32'
+                            },
+                            'fields': [{
+                                'name': 'Command',
+                                'value': '`/' + `${command.name}` + '`',
+                                'inline': false
+                            }, {
+                                'name': 'Time Remaining',
+                                'value': '`' + `${timeLeft.toFixed(1)}` + ' second(s)`',
+                                'inline': false
+                            }]
+                        };
+                        let response = await message.channel.send({
+                            embed: noargs
+                        })
+                        response.delete({
+                            timeout: 5000
+                        })
+                    });
+                return;
+            }
+        }
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        try {
+            command.execute(message, args, client);
+        } catch (error) {
+            console.error(error);
+            message.reply('Error, please report');
+        }
+    }
 })
-
+/**
 client.on('channelCreate', async channel => {
     client.logging.get('channelCreate').execute(client, channel)
 })
@@ -257,7 +358,7 @@ client.on('stickerDelete', async sticker => {
 client.on('stickerUpdate', async sticker => {
     client.logging.get('stickerUpdate').execute(client, sticker)
 })
-
+**/
 
 
 client.login('OTgzMjYwNTMwMDExNjkzMDU2.GpqLue.Q4vTBEfQgwbf5u4ssqgLjpANXZuaZd3ql8GWio')
---
+++ vanix/main.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const assets = require("../config/assets.json");
+const fetch = require("node-fetch");
+const chatcheck = require("./chatcheck.json")
+
+module.exports = {
+    name: "main",
+    description: "the core for vanix",
+    async execute(client, message) {
+		if(message.content.startsWith("//")){return}
+		var modifiedmessage = message.content
+		//replace items in message
+		for(const x in chatcheck.replace){
+			if(modifiedmessage.includes(x)){
+				modifiedmessage = modifiedmessage.replaceAll(x,chatcheck.replace[x])
+			}
+		}
+		//Language barrier
+		for(const x in chatcheck.language){
+			if(modifiedmessage.includes(" "+x+" ")){
+				modifiedmessage = modifiedmessage.replaceAll(x,chatcheck.language[x])
+			}
+		}
+		//Manual override for answer
+		for(const x in chatcheck.custom){
+			if(modifiedmessage===x){
+				return message.reply(chatcheck.custom[x])
+			}
+		}
+		
+        var json = await fetch("https://api.affiliateplus.xyz/api/chatbot?message="+modifiedmessage+"&name=Vanix&user=siryakstudioschatbot"+message.author.id+"&botmaster=<@462554041440796674>&master=<@462554041440796674>")
+		var chat = await json.json()
+		message.reply(chat.message)
+    }
+};
